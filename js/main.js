1940cdde2082620e59fee67c9140f4e5822fc35c
Smith Jesko <smithjesko@gmail.com>
2018-04-29 15:31:41 -0500

    Minor changes

--- js/main.js
+++ js/main.js
@@ -9,10 +9,10 @@ $("#toggle").click(function() {
     $("#resize").toggleClass("active");
 });
 
-var btn = document.querySelector('.mouse-cursor-gradient-tracking')
-btn.onmousemove = function(e) {
-  var x = e.pageX - btn.offsetLeft
-  var y = e.pageY - btn.offsetTop
-  btn.style.setProperty('--x', x + 'px')
-  btn.style.setProperty('--y', y + 'px')
-}
+// var btn = document.querySelector('.mouse-cursor-gradient-tracking')
+// btn.onmousemove = function(e) {
+//   var x = e.pageX - btn.offsetLeft
+//   var y = e.pageY - btn.offsetTop
+//   btn.style.setProperty('--x', x + 'px')
+//   btn.style.setProperty('--y', y + 'px')
+// }
